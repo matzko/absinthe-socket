b4aa38ba91a2416bbb6e8941f3126e0cb9e5f449
Austin Matzko <austin@pressedcode.com>
2023-01-07 10:02:40 -0500

    Removing build stuff I don't want.

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    "@commitlint/config-conventional",
-    "@commitlint/config-lerna-scopes"
-  ],
-  rules: {
-    "subject-case": [2, "always", "sentence-case"]
-  }
-};
